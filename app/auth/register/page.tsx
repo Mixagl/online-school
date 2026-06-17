1e847b8ae2a925ea61c37993984886ddd01f429b
mixagl <105538023+Mixagl@users.noreply.github.com>
2026-06-17 12:42:59 +1000

    feat: add login and register pages

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,147 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { Controller, useForm } from "react-hook-form";
+import z from "zod";
+
+const formSchema = z.object({
+  name: z.string().min(3, "Минимальная длина имени 3 символа"),
+  email: z.string().email("Некорректный email"),
+  password: z.string().min(8, "Пароль должен быть минимум 8 символов"),
+});
+
+export default function RegisterPage() {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(formData: z.infer<typeof formSchema>) {
+    const { error } = await authClient.signUp.email({
+      email: formData.email,
+      name: formData.name,
+      password: formData.password,
+    });
+
+    if (error) {
+      console.error(error.message || "Ошибка регистрации");
+    } else {
+      console.log("Аккаунт создан");
+      router.push("/");
+    }
+  }
+
+  return (
+    <section className="w-full bg-background pt-32 pb-32">
+      <div className="mx-auto container px-5 max-w-2xl">
+        <Card>
+          <CardHeader>
+            <CardTitle>Регистрация</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form id="register-form" onSubmit={form.handleSubmit(onSubmit)}>
+              <FieldGroup>
+                {/* Имя */}
+                <Controller
+                  name="name"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field data-invalid={fieldState.invalid}>
+                      <FieldLabel htmlFor="name">Имя</FieldLabel>
+                      <Input
+                        {...field}
+                        id="name"
+                        aria-invalid={fieldState.invalid}
+                        placeholder="Введите свое имя"
+                        autoComplete="off"
+                      />
+                      {fieldState.invalid && (
+                        <FieldError errors={[fieldState.error]} />
+                      )}
+                    </Field>
+                  )}
+                />
+                {/* Email */}
+                <Controller
+                  name="email"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field data-invalid={fieldState.invalid}>
+                      <FieldLabel htmlFor="email">Email</FieldLabel>
+                      <Input
+                        {...field}
+                        id="email"
+                        aria-invalid={fieldState.invalid}
+                        placeholder="Введите свой email"
+                        autoComplete="off"
+                      />
+                      {fieldState.invalid && (
+                        <FieldError errors={[fieldState.error]} />
+                      )}
+                    </Field>
+                  )}
+                />
+                {/* Пароль */}
+                <Controller
+                  name="password"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field data-invalid={fieldState.invalid}>
+                      <FieldLabel htmlFor="password">Пароль</FieldLabel>
+                      <Input
+                        {...field}
+                        id="password"
+                        aria-invalid={fieldState.invalid}
+                        placeholder="Введите свой пароль"
+                        autoComplete="off"
+                      />
+                      {fieldState.invalid && (
+                        <FieldError errors={[fieldState.error]} />
+                      )}
+                    </Field>
+                  )}
+                />
+              </FieldGroup>
+            </form>
+          </CardContent>
+          <CardFooter>
+            <Field orientation={"horizontal"}>
+              <Button
+                type="button"
+                variant={"outline"}
+                onClick={() => form.reset()}
+              >
+                Сбросить
+              </Button>
+              <Button type="submit" form="register-form">
+                Зарегистрироваться
+              </Button>
+            </Field>
+          </CardFooter>
+        </Card>
+      </div>
+    </section>
+  );
+}
